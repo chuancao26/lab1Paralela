d5f9aaed76ea53aaa4d85cb97731207475fecbaa
chuancao26 <chuancao@unsa.edu.pe>
2025-09-14 17:26:07 -0500

    adding first exercice code

--- dobleBucleFor.cpp
+++ dobleBucleFor.cpp
@@ -1,30 +1,40 @@
 #include <iostream>
-#include <ctime>
 using namespace std;
 #define MAX 100
+void print(double a[])
+{
+  for(int i = 0; i < MAX; i++)
+  {
+    cout << a[i] << endl;
+  }
+}
 int main()
 {
   double A[MAX][MAX], y[MAX], x[MAX];
   for (int i = 0; i < MAX; i++)
   {
-    x[i] = rand() % 3;
+    x[i] = (double)rand() / RAND_MAX;
     y[i] = 0.0;
     for (int j = 0; j < MAX; j++)
     {
-      A[i][j] = rand() % 3; 
+      A[i][j] = (double)rand() / RAND_MAX;
     }
   }
 
   for (int i = 0; i < MAX; i++)
   {
-    for(int j = 0; i < MAX; j++)
+    for(int j = 0; j < MAX; j++)
     {
       y[i] += A[i][j] * x[j];
     }
   }
 
-
-
-
+  for (int j = 0; j < MAX; j++)
+  {
+    for(int i = 0; i < MAX; i++)
+    {
+      y[i] += A[i][j] * x[j];
+    }
+  }
   return 0;
 }
